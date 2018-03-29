2daa893af61cda898ba2689e03d90b062f91255a
Horia Coman <horia141@gmail.com>
2018-03-29 09:35:06 +0300

    Remove the "clients" argument to api server middleware

--- src/common-api-server-middleware-test.ts
+++ src/common-api-server-middleware-test.ts
@@ -1,5 +1,3 @@
-import { expect } from 'chai'
-import * as HttpStatus from 'http-status-codes'
 import 'mocha'
 import * as td from 'testdouble'
 
@@ -7,8 +5,8 @@ import { newCommonApiServerMiddleware } from './common-api-server-middleware'
 
 
 describe('CommonApiServerMiddleware', () => {
-    it('should allow request with proper origin and set json type', (done) => {
-        const middleware = newCommonApiServerMiddleware(['truesparrow.com']);
+    it('should set json type', (done) => {
+        const middleware = newCommonApiServerMiddleware();
 
         const mockReq = td.object({
             header: (_name: string) => { },
@@ -16,81 +14,9 @@ describe('CommonApiServerMiddleware', () => {
         });
         const mockRes = td.object(['type']);
 
-        td.when(mockReq.header('Origin')).thenReturn('truesparrow.com');
-
         middleware(mockReq as any, mockRes as any, () => {
             td.verify(mockRes.type('json'));
             done();
         });
-    });
-
-    it('should allow request with proper origin out of multiple ones', (done) => {
-        const middleware = newCommonApiServerMiddleware(['truesparrow.com', 'truesparrow.io']);
-
-        const mockReq = td.object({
-            header: (_name: string) => { },
-            headers: {}
-        });
-        const mockRes = td.object('Response');
-
-        td.when(mockReq.header('Origin')).thenReturn('truesparrow.com');
-
-        middleware(mockReq as any, mockRes as any, () => {
-            done();
-        });
-    });
-
-    it('should block a request with a disallowed origin', () => {
-        const middleware = newCommonApiServerMiddleware(['truesparrow.com']);
-        let passedCheck = false;
-
-        /* codecov skip start */
-        const mockReq = td.object({
-            header: (_name: string) => { },
-            headers: {},
-            log: { warn: (_msg: string) => { } }
-        });
-        /* codecov skip end */
-        const mockRes = td.object(['status', 'end']);
-
-        td.when(mockReq.header('Origin')).thenReturn('truesparrow.io');
-
-        middleware(mockReq as any, mockRes as any, () => { passedCheck = true; });
-
-        expect(passedCheck).to.be.false;
-        td.verify(mockReq.log.warn('Origin is not allowed'));
-        td.verify(mockRes.status(HttpStatus.BAD_REQUEST));
-        td.verify(mockRes.end());
-    });
-
-    it('should copy the list of clients', () => {
-        const allowedOrigins = ['truesparrow.com'];
-        const middleware = newCommonApiServerMiddleware(allowedOrigins);
-
-        const mockReq = td.object({
-            header: (_name: string) => { },
-            headers: {}
-        });
-        const mockRes = td.object('Response');
-
-        td.when(mockReq.header('Origin')).thenReturn('truesparrow.com');
-
-        {
-            var passedCheck = false;
-
-            middleware(mockReq as any, mockRes as any, () => { passedCheck = true });
-
-            expect(passedCheck).to.be.true;
-        }
-
-        allowedOrigins[0] = 'truesparrow.io';
-
-        {
-            var passedCheck = false;
-
-            middleware(mockReq as any, mockRes as any, () => { passedCheck = true });
-
-            expect(passedCheck).to.be.true;
-        }
     });
 });
--- src/common-api-server-middleware.ts
+++ src/common-api-server-middleware.ts
@@ -3,7 +3,6 @@
 /** Imports. Also so typedoc works correctly. */
 import * as bodyParser from 'body-parser'
 import * as express from 'express'
-import * as HttpStatus from 'http-status-codes'
 
 import { Request } from './request'
 
@@ -17,27 +16,15 @@ import { Request } from './request'
  * @note A limit of 100kb is placed on inputs.
  * @remark The check is textual, so anyone can fake such a request if they're in the proper
  * position, but it is a start to identity based checks.
- * @param clients - an array of hostnames, indicating the allowed services, both client and server
  *     which can contact this API.
  * @returns an {@link express.RequestHandler} which does all of the above.
  */
-export function newCommonApiServerMiddleware(clients: string[]): express.RequestHandler {
-    const localClients = clients.slice(0);
+export function newCommonApiServerMiddleware(): express.RequestHandler {
     const bodyParserMiddleware = bodyParser.json({ limit: '100kb' });
 
     return (req: Request, res: express.Response, next: express.NextFunction) => {
         bodyParserMiddleware(req, res, () => {
-            const origin = req.header('Origin') as string;
-
-            if (localClients.indexOf(origin) == -1) {
-                req.log.warn('Origin is not allowed');
-                res.status(HttpStatus.BAD_REQUEST);
-                res.end();
-                return;
-            }
-
             res.type('json');
-
             // Fire away.
             next();
         });
